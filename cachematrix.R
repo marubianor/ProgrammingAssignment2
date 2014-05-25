f4a8e22ea62330cbebd8a673ec24dbb8eaffc151
marubianor <marubianor@unal.edu.co>
2014-05-25 17:44:08 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {  require("MASS")
-  m_inverse <- NULL
+  m_inverse <- NULL ##Asigna el valor nulo a la funcion cuando tome valores indefinidos
   set <- function(y) {
     x <<- y
     m_inverse <<- NULL
